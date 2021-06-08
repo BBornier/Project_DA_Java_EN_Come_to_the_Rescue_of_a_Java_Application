084373ec2b59e2edb4ee9c85706dbccbe96ff1c5
Benoît B <benoitbornier@yahoo.fr>
2021-06-08 10:40:09 +0200

    Classe avec méthode de tri. Lit le fichier symptoms.txt, appelle la méthode getSymptoms(), utilise la collection TreeMap (tri par ordre naturel les clés) pour classer/trier chaque doublon par ordre alphabétique et en comptant le nombre d'occurences par symptôme.

--- Project02Eclipse/src/com/hemebiotech/analytics/SortSymptomsData.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SortSymptomsData.java
@@ -3,47 +3,48 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.TreeMap;
 
-	/**
-	 * Here the SortSymptomData class will use the readSymptomData Class and its methods.
-	 * It will count occurrences and sort data in an alphabetical order.  
-	 * 
-	 * @see ReadSymptomDataFromFile
-	 * 
-	 * @author BBornier
-	 * @version 5.0
-	 *
-	 */
+/**
+ * Here the SortSymptomData class will use the ReadSymptomDataFromFile Class and
+ * its methods. It will count occurrences and sort symptoms in an alphabetical
+ * order.
+ * 
+ * @see ReadSymptomDataFromFile
+ * 
+ * @see ISymptomReader
+ * 
+ * @author BBornier
+ * 
+ * @version 5.0
+ * 
+ */
 public class SortSymptomsData {
-	
-	/** 
-	 * 
-	 * It will read the symptoms file,
-	 * @see BufferReader
-	 * 
-	 * put it in a list,
-	 * @see List
-	 * 
-	 * sort the list in alphabetical order and with occurrences number for each symptom.
-	 * It uses symptomsTri object.
-	 * 
-	 * @return symptomsTri
-	 *  
-	 */
 
+	/**
+	 * @return symptomsSort. Sort the list in alphabetical order, with occurrences
+	 *         number for each symptom. It uses symptomsSort object.
+	 */
 	public TreeMap<String, Integer> sortingSymptomsData() {
-		
+
 		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
-		file.getSymptoms();
 		List<String> symptomsFile = file.getSymptoms();
 
-		TreeMap<String, Integer> symptomsTri = new TreeMap<>();
+		TreeMap<String, Integer> symptomsSort = new TreeMap<>();
 		for (String listSymptoms : symptomsFile) {
-			if (symptomsTri.containsKey(listSymptoms)) {
-				symptomsTri.put(listSymptoms, symptomsTri.get(listSymptoms) + 1);
+			
+	/**
+	* If the line contained the same line/symptom, increment the associate line :
+	*/
+			if (symptomsSort.containsKey(listSymptoms)) {
+				symptomsSort.put(listSymptoms, symptomsSort.get(listSymptoms) + 1);
+	
+	/**
+	* Else, pass to the next line, add it and initialize the count to 1 :
+	*/
 			} else {
-				symptomsTri.put(listSymptoms, 1);
+				symptomsSort.put(listSymptoms, 1);
 			}
+
 		}
-			return symptomsTri;
+		return symptomsSort;
 	}
 }
