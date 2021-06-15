a29de57212aa8d639f4a25b9eddee4bd3bf1866c
Benoît B <benoitbornier@yahoo.fr>
2021-06-15 10:52:18 +0200

    Fonctionnalité d'ajout de symptôme dans le fichier symptoms.txt développée. Problème de FileNotFoundException à résoudre. Condition if fonctionne.

--- Project02Eclipse/src/com/hemebiotech/analytics/AddSymptom.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AddSymptom.java
@@ -3,39 +3,44 @@ package com.hemebiotech.analytics;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class AddSymptom {
 	
-	public void addSymptoms () throws IOException {
+	public static void addSymptoms () {
 		
-		String answer = "Y";
-		String symptom;
-		String filename = "./symptoms.txt";
-		File symptomFile;
+		File symptomFile = new File("D:\\Travail\\Formations\\Dev Appli Java\\P2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
+		String absolutePath = symptomFile.getAbsolutePath();
+		
+		//URL symptoms = ReadSymptomDataFromFile.class.getClassLoader().getResource("symptoms.txt");
+		//File symptomFile = new File(symptoms.toString());
 		
 		Scanner sc = new Scanner(System.in);
-		symptomFile = new File(filename);
 		
-		System.out.println("\n" + "Do you want to add a new symptoms in symptoms.txt ? (Y/N)");
-		answer = sc.nextLine();
+		System.out.println("\n" + "Do you want to add a new symptoms in symptoms.txt ? (Yes/No)");
+		String answer = sc.nextLine();
 		
-		if (answer == "Y") {
-			System.out.println("Please, write and enter the new symptom :");
-			symptom = sc.nextLine();
+		if (answer.equals("Yes")) 
+		{
+			System.out.println("\n" + "Please, write and enter the new symptom :");
+			String symptom = sc.nextLine();
 			try {
-				FileWriter newSymptom = new FileWriter(filename + "\r\n", true);
+				FileWriter newSymptom = new FileWriter(symptomFile + "\r\n", true);
 				newSymptom.write(symptom);
 				newSymptom.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 
 			}
-			
-		} else {
-			System.out.println("Thank you for using our HemeBiotech symptoms application. Have a good day.");
+			System.out.println("\n" + "Thank you for using our HemeBiotech symptoms application. Have a good day.");
 		}
-			
+			else if (answer.equals("No"))
+		{
+			System.out.println("\n" + "No problem. Have a good day.");
+		}
+			else
+				System.out.println("\n" + "Wrong answer, please launch again the program.");
 	}
 
 }
