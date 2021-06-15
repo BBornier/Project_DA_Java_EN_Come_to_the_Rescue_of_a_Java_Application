ffcb652aee378ee536d62617a98981b1ace76c72
Benoît B <benoitbornier@yahoo.fr>
2021-06-15 16:55:27 +0200

    Fonctionnalité perfectionnée.Boucle Do While intégrée pour la demande d'ajout de nouveaux symptômes.

--- Project02Eclipse/src/com/hemebiotech/analytics/AddSymptom.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AddSymptom.java
@@ -3,24 +3,41 @@ package com.hemebiotech.analytics;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
 import java.util.Scanner;
 
+/**
+ * AddSymptoms to add new symptoms to the file symptoms.txt. Not asked to be created but it's an idea. 
+ *
+ * @author BBornier
+ * 
+ * @version 1.0
+ */
 public class AddSymptom {
-	
-	public static void addSymptoms () {
-		
-		File symptomFile = new File("D:\\Travail\\Formations\\Dev Appli Java\\P2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
+	/**
+	 * Method addSymptoms() asks to the User if he wants to add a new symptom to the symptoms.txt file.
+	 * A do while loop allow to leave the application or stay to continue to add new symptoms.
+	 * New Symptoms are writen in symptoms.txt file. Then the application continue to sort new symptoms and add occurrences.
+	 * 
+	 * @see SortSymptomsData
+	 * @see FileWriterData
+	 * 
+	 */
+	public void addSymptoms() {
+
+		File symptomFile = new File("Project02Eclipse/symptoms.txt");
 		String absolutePath = symptomFile.getAbsolutePath();
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("\n" + "Do you want to add a new symptoms in symptoms.txt ? (Yes/No)");
-		String answer = sc.nextLine();
-		
-		if (answer.equals("Yes")) 
-		{
-			System.out.println("\n" + "Please, write and enter the new symptom :");
+		String answer = "Yes";
+
+		do {
+			System.out.println("\n" + "Do you want to add another symptom in symptoms.txt ? (Yes/No)");
+			answer = sc.nextLine();
+			if (answer.equalsIgnoreCase("No")) {
+				System.out.println("\n" + "No problem. Have a good day.");
+				break;
+			} else
+				System.out.println("Please, write and enter the new symptom :");
 			String symptom = sc.nextLine();
 			try {
 				FileWriter newSymptom = new FileWriter(symptomFile, true);
@@ -29,16 +46,9 @@ public class AddSymptom {
 				newSymptom.close();
 			} catch (IOException e) {
 				e.printStackTrace();
-
 			}
-			System.out.println("\n" + "Thank you for using our HemeBiotech symptoms application. Have a good day.");
-		}
-			else if (answer.equals("No"))
-		{
-			System.out.println("\n" + "No problem. Have a good day.");
-		}
-			else
-				System.out.println("\n" + "Wrong answer, please launch again the program.");
-	}
 
+		} while (answer.equalsIgnoreCase("Yes"));
+		System.out.println("\n" + "Thank you for using our HemeBiotech symptoms application.");
+	}
 }
